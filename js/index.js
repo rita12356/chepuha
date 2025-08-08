b312e6e37f56554bdcbed7a5f3c6414ea356a378
rita12356 <wadeew2016@yandex.ru>
2025-08-08 15:50:41 +0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let labelNode = document.querySelector('#label');
+let answerNode = document.querySelector(`#answer`);
+let stotyNode = document.querySelector(`#story`);
+let nextNode = document.querySelector(`#next`);
+let questionsNode = document.querySelector(`#questions`);
+
+let counter = 0;
+let story = ``;
+
+nextNode.addEventListener(`click`, function() {
+    counter++;
+    if(counter == 1) {
+        story = answerNode.value;
+        labelNode.innerHTML = `Где?`;
+        
+    } else if(counter == 2){
+        story = `${story} ${answerNode.value}`;
+        labelNode.innerHTML = `С кем?`;
+    } else if(counter == 3) {
+        story = `${story} ${answerNode.value}`;
+        labelNode.innerHTML = `Что делали?`;
+    } else if(counter == 4) {
+        story = `${story} ${answerNode.value}`;
+        labelNode.innerHTML = `Зачем?`;
+    } else if(counter == 5) {
+        story = `${story}, ${answerNode.value}`;
+        labelNode.innerHTML = `Чем дело закончилось?`; 
+    } else{
+        questionsNode.classList.add(`d-none`);
+        stotyNode.innerHTML = story + `.` + answerNode.value + `.`
+    }
+
+    answerNode.value = ``;
+});
